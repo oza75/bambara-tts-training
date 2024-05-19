d1ec831928f486899be98e00d66498ba1ab916b3
Aboubacar Ouattara <abouba181@gmail.com>
2024-05-18 20:57:08 -0400

    wip

--- custom_xtts/vq_vae/models.py
+++ custom_xtts/vq_vae/models.py
@@ -5,9 +5,10 @@ from diffusers import VQModel
 from diffusers.models.autoencoders.vae import DecoderOutput
 from diffusers.models.vq_model import VQEncoderOutput
 from diffusers.utils.accelerate_utils import apply_forward_hook
+from transformers import PretrainedConfig, PreTrainedModel
 
 
-class SpeechVQVAE(VQModel):
+class VQVAE(VQModel):
     """
     A VQ-VAE model with support for attention masks for decoding latent representations.
 
@@ -25,13 +26,15 @@ class SpeechVQVAE(VQModel):
         return VQEncoderOutput(latents=h_q)
 
     def forward(
-            self, sample: torch.FloatTensor, attention_mask: Optional[torch.Tensor] = None, return_dict: bool = True
+            self, input_values: torch.FloatTensor,
+            attention_mask: Optional[torch.Tensor] = None,
+            return_dict: bool = True
     ) -> Union[DecoderOutput, Tuple[torch.FloatTensor, ...]]:
         """
         The forward method with attention mask support.
 
         Args:
-            sample (`torch.FloatTensor`): Input sample.
+            input_values (`torch.FloatTensor`): Input values.
             attention_mask (`torch.Tensor`, *optional*): Attention mask for the input sample.
             return_dict (`bool`, *optional*, defaults to `True`):
                 Whether or not to return a `DecoderOutput` instead of a plain tuple.
@@ -40,7 +43,12 @@ class SpeechVQVAE(VQModel):
             `DecoderOutput` or `tuple`:
                 If return_dict is True, a `DecoderOutput` is returned, otherwise a plain `tuple` is returned.
         """
-        h_e, h_q = self.encode(sample, return_dict=return_dict)
+        encoder_outputs = self.encode(input_values, return_dict=return_dict)
+
+        if not return_dict:
+            h_e, h_q = encoder_outputs
+        else:
+            h_q = encoder_outputs.latents
 
         dec = self.decode(h_q).sample
 
@@ -48,3 +56,69 @@ class SpeechVQVAE(VQModel):
             return dec, h_q, h_e
 
         return DecoderOutput(sample=dec)
+
+
+class SpeechVQConfig(PretrainedConfig):
+    model_type = "speech_vq_vae"
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.in_channels = kwargs.get("in_channels", 1)
+        self.out_channels = kwargs.get("out_channels", 1)
+        self.down_block_types = kwargs.get("down_block_types", ("DownEncoderBlock2D",))
+        self.up_block_types = kwargs.get("up_block_types", ("UpDecoderBlock2D",))
+        self.block_out_channels = kwargs.get("block_out_channels", (64,))
+        self.layers_per_block = kwargs.get("layers_per_block", 1)
+        self.act_fn = kwargs.get("act_fn", "silu")
+        self.latent_channels = kwargs.get("latent_channels", 1)
+        self.sample_size = kwargs.get("sample_size", 32)
+        self.num_vq_embeddings = kwargs.get("num_vq_embeddings", 256)
+        self.norm_num_groups = kwargs.get("norm_num_groups", 32)
+        self.vq_embed_dim = kwargs.get("vq_embed_dim", None)
+        self.scaling_factor = kwargs.get("scaling_factor", 0.18215)
+        self.norm_type = kwargs.get("norm_type", "group")
+
+
+class SpeechVQVAE(PreTrainedModel):
+    config_class = SpeechVQConfig
+
+    def __init__(self, config: SpeechVQConfig, model: Optional[VQVAE] = None):
+        super().__init__(config)
+        self.model = model if model is not None else VQVAE(
+            in_channels=config.in_channels,
+            out_channels=config.out_channels,
+            down_block_types=config.down_block_types,
+            up_block_types=config.up_block_types,
+            block_out_channels=config.block_out_channels,
+            layers_per_block=config.layers_per_block,
+            act_fn=config.act_fn,
+            latent_channels=config.latent_channels,
+            sample_size=config.sample_size,
+            num_vq_embeddings=config.num_vq_embeddings,
+            norm_num_groups=config.norm_num_groups,
+            vq_embed_dim=config.vq_embed_dim,
+            scaling_factor=config.scaling_factor,
+            norm_type=config.norm_type
+        )
+
+    @apply_forward_hook
+    def encode(self, x: torch.FloatTensor, return_dict: bool = True) -> Union[Tuple[Any, Any], VQEncoderOutput]:
+        return self.model.encode(x, return_dict=return_dict)
+
+    def forward(
+            self, input_values: torch.FloatTensor,
+            labels: torch.FloatTensor = None,
+            attention_mask: Optional[torch.Tensor] = None,
+            return_dict: bool = False
+    ) -> Union[DecoderOutput, Tuple[torch.FloatTensor, ...]]:
+        return self.model(input_values, attention_mask=attention_mask, return_dict=return_dict)
+
+    def save_pretrained(self, save_directory, **kwargs):
+        super().save_pretrained(save_directory, **kwargs)
+        self.model.save_pretrained(save_directory, **kwargs)
+
+    @classmethod
+    def from_pretrained(cls, pretrained_model_name_or_path, *model_args, **kwargs):
+        config = SpeechVQConfig.from_pretrained(pretrained_model_name_or_path, **kwargs)
+        model = VQVAE.from_pretrained(pretrained_model_name_or_path, *model_args, **kwargs)
+        return cls(config, model)
--- custom_xtts/vq_vae/train_vq_vae.py
+++ custom_xtts/vq_vae/train_vq_vae.py
@@ -1,48 +1,68 @@
 import torch
 import torch.nn.functional as F
 from datasets import load_dataset
-from transformers import HfArgumentParser, TrainingArguments, Trainer, EvalPrediction, TrainerCallback
-import evaluate
-from .features import VQVAEProcessor, VQVAEFeatureExtractor
-from .models import SpeechVQVAE
+from transformers import TrainingArguments, Trainer, EvalPrediction, TrainerCallback
+from transformers.integrations import WandbCallback
+from features import VQVAEProcessor, VQVAEFeatureExtractor
+from models import SpeechVQVAE, SpeechVQConfig
 import wandb
+import numpy as np
+from matplotlib import pyplot as plt
 
 
-class AudioLoggingCallback(TrainerCallback):
-    def on_epoch_end(self, args, state, control, **kwargs):
+class AudioLoggingCallback(WandbCallback):
+    def on_evaluate(self, args, state, control, **kwargs):
+        if not state.is_world_process_zero:
+            return
+
         # Ensure the trainer and model are available
-        if not hasattr(self, "trainer"):
-            self.trainer = kwargs.get("model")
+        model = kwargs.get("model")
+        eval_dataloader = kwargs.get("eval_dataloader")
 
-        if not hasattr(self, "trainer"):
+        if eval_dataloader is None or model is None:
+            print(f"Either eval_dataloader or model aren't provided: model {model}, eval_dataloader {eval_dataloader}")
             return
 
         # Get a batch from the validation dataset
-        eval_dataloader = self.trainer.get_eval_dataloader()
         batch = next(iter(eval_dataloader))
 
         # Move batch to the correct device
-        inputs = batch["input_values"].to(self.trainer.model.device)
-        attention_masks = batch["attention_mask"].to(self.trainer.model.device)
+        inputs = batch["input_values"].to(model.device)
+        attention_masks = batch["attention_mask"].to(model.device)
 
         # Get model outputs
         with torch.no_grad():
-            outputs = self.trainer.model(inputs)
+            outputs = model(inputs, return_dict=True)
 
-        # Apply attention masks to original and reconstructed audios
+        # Apply attention masks to original and reconstructed mel spectrograms
         inputs_masked = inputs * attention_masks
         outputs_masked = outputs.sample * attention_masks
 
-        # Log original and reconstructed audios
+        # Log original and reconstructed mel spectrograms
         for idx in range(min(5, inputs.size(0))):  # Log up to 5 samples
-            original_audio = inputs_masked[idx].cpu().numpy().flatten()
-            reconstructed_audio = outputs_masked[idx].cpu().numpy().flatten()
+            original_mel = inputs_masked[idx].cpu().squeeze(0).numpy()
+            reconstructed_mel = outputs_masked[idx].squeeze(0).cpu().numpy()
+
+            # Extract valid sections based on the attention mask
+            valid_length = int(attention_masks[idx].squeeze(0)[0].sum().item())
+            original_mel_valid = original_mel[:, :valid_length]
+            reconstructed_mel_valid = reconstructed_mel[:, :valid_length]
+
+            # Plot and save the mel spectrograms
+            fig, axs = plt.subplots(2, 1, figsize=(10, 6))
+            axs[0].imshow(original_mel_valid, aspect='auto', origin='lower')
+            axs[0].set_title('Original Mel Spectrogram')
+            axs[1].imshow(reconstructed_mel_valid, aspect='auto', origin='lower')
+            axs[1].set_title('Reconstructed Mel Spectrogram')
+            plt.tight_layout()
 
             # Log using wandb
-            wandb.log({
-                f"original_audio_{idx}": wandb.Audio(original_audio, sample_rate=22050),
-                f"reconstructed_audio_{idx}": wandb.Audio(reconstructed_audio, sample_rate=22050)
+            self._wandb.log({
+                f"mel_spectrogram_{idx}": wandb.Image(fig)
             })
+
+            # Close the plot to free memory
+            plt.close(fig)
 
 
 def masked_vq_vae_loss(x, x_recon, z_e, z_q, attention_mask, beta=0.25):
@@ -91,8 +111,20 @@ def compute_metrics(eval_pred):
     Returns:
         dict: A dictionary containing the computed metrics.
     """
-    logits, (labels, attention_masks) = eval_pred.predictions, eval_pred.label_ids
+    (logits, attention_masks), labels = eval_pred.predictions, eval_pred.label_ids
 
+    print(f"Logits type: {type(logits)}, Labels type: {type(labels)}, Attention masks type: {type(attention_masks)}")
+    print(f"Logits shape: {logits.shape}, Labels shape: {labels.shape}, Attention masks shape: {attention_masks.shape}")
+
+    # Ensure logits, labels, and attention_masks are tensors
+    if isinstance(logits, np.ndarray):
+        logits = torch.tensor(logits)
+    if isinstance(labels, np.ndarray):
+        labels = torch.tensor(labels)
+    if isinstance(attention_masks, np.ndarray):
+        attention_masks = torch.tensor(attention_masks)
+
+    print(f"Compute Metrics: {labels.shape, attention_masks.shape}")
     # Apply the attention masks
     logits = logits * attention_masks
     labels = labels * attention_masks
@@ -115,9 +147,15 @@ class DataCollator:
     """
 
     def __call__(self, features):
-        input_values = torch.stack([f["input_values"] for f in features])
-        attention_mask = torch.stack([f["attention_mask"] for f in features])
-        return {"input_values": input_values, "attention_mask": attention_mask}
+        input_values = torch.stack([torch.tensor(f["input_values"], dtype=torch.float32) for f in features])
+        attention_mask = torch.stack([torch.tensor(f["attention_mask"], dtype=torch.float32) for f in features])
+
+        batch_size, n_mels, time_steps = input_values.shape
+
+        input_values = input_values.view(batch_size, 1, n_mels, time_steps)
+        attention_mask = attention_mask.view(batch_size, 1, n_mels, time_steps)
+
+        return {"input_values": input_values, "labels": input_values, "attention_mask": attention_mask}
 
 
 def preprocess_function(examples, processor):
@@ -132,10 +170,33 @@ def preprocess_function(examples, processor):
         dict: The preprocessed examples with input values and attention masks.
     """
     batch = processor(examples)
+
     return {
         "input_values": batch["mel_spectrogram"],
         "attention_mask": batch["attention_mask"]
     }
+
+
+class CustomTrainer(Trainer):
+    def compute_loss(self, model, inputs, return_outputs=False):
+        input_values = inputs["input_values"]
+        attention_mask = inputs["attention_mask"]
+
+        # Forward pass
+        outputs = model(input_values, attention_mask=attention_mask)
+        logits = outputs[0]
+
+        # Compute custom loss
+        loss = masked_vq_vae_loss(
+            x=input_values,
+            x_recon=logits,
+            z_e=outputs[2],
+            z_q=outputs[1],
+            attention_mask=attention_mask,
+            beta=0.25
+        )
+
+        return (loss, outputs) if return_outputs else loss
 
 
 def main():
@@ -150,55 +211,44 @@ def main():
     processor = VQVAEProcessor(feature_extractor)
 
     # Preprocess the datasets
-    dataset = dataset.map(lambda examples: preprocess_function(examples, processor), batched=True,  num_proc=8)
+    dataset = dataset.map(lambda examples: preprocess_function(examples, processor), batched=True, num_proc=8)
 
     train_dataset = dataset["train"]
     eval_dataset = dataset["test"]
 
+    config = SpeechVQConfig()
+
     # Define the VQ-VAE model
-    model = SpeechVQVAE(
-        in_channels=1,
-        out_channels=1,
-        down_block_types=("DownEncoderBlock2D",),
-        up_block_types=("UpDecoderBlock2D",),
-        block_out_channels=(64,),
-        layers_per_block=1,
-        act_fn="silu",
-        latent_channels=1,
-        sample_size=32,
-        num_vq_embeddings=256,
-        norm_num_groups=32,
-        vq_embed_dim=None,
-        scaling_factor=0.18215,
-        norm_type="group"
-    )
+    model = SpeechVQVAE(config)
 
     # Define training arguments
     training_args = TrainingArguments(
         output_dir="./results",
-        evaluation_strategy="steps",
-        eval_steps=500,
+        eval_strategy="epoch",
+        eval_steps=1,
         save_steps=1000,
         learning_rate=5e-5,
-        per_device_train_batch_size=64,
-        per_device_eval_batch_size=32,
+        per_device_train_batch_size=16,
+        per_device_eval_batch_size=8,
         num_train_epochs=3,
         weight_decay=0.01,
         optim="adamw_torch_fused",
-        fp16=True,
-        deepspeed="./deepspeed_config.json",
+        tf32=True,
+        deepspeed="../deepspeed_config.json",
         report_to=['tensorboard', 'wandb'],
     )
 
+    data_collator = DataCollator()
+
     # Initialize the Trainer
-    trainer = Trainer(
+    trainer = CustomTrainer(
         model=model,
         args=training_args,
         train_dataset=train_dataset,
         eval_dataset=eval_dataset,
-        data_collator=DataCollator(),
+        data_collator=data_collator,
         compute_metrics=compute_metrics,
-        callbacks=[AudioLoggingCallback]
+        # callbacks=[AudioLoggingCallback]
     )
 
     # Start training
